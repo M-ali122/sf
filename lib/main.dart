8e193e8fd43f80a8c989fd74014f753f458e9759
zain shaikh <139116312+zainshaikh017@users.noreply.github.com>
2024-03-23 11:53:45 +0500

    pull request

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:sf_app/modules/global/controllers/bindings.dart';
 import 'package:sf_app/pages/message/view/inbox.dart';
+import 'package:sf_app/pages/splash/view/splash_view.dart';
 import 'package:sf_app/resources/languages/dictionary.dart';
 import 'package:sf_app/resources/routes/Approute.dart';
 import 'package:sf_app/resources/theme/main_theme.dart';
@@ -32,8 +33,8 @@ class MainApp extends StatelessWidget {
           translations: AppDictionary(),
           // home: child,
           routes: AppRoute().routes,
-          // initialRoute: Splash_View.route,
-          initialRoute: Inbox.route,
+          initialRoute: Splash_View.route,
+          // initialRoute: Inbox.route,
         );
       },
     );
